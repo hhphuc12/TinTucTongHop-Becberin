92ea985eb509ec66428ac34beea7454fbe207d30
GNPC\giapn <giapnguyenitf@gmail.com>
2017-05-10 19:43:24 +0700

    main activity

--- app/src/main/java/com/phuc/tintuctonghop/MainActivity.java
+++ app/src/main/java/com/phuc/tintuctonghop/MainActivity.java
@@ -1,13 +1,129 @@
 package com.phuc.tintuctonghop;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    ListView lv;
+    ArrayList<NavigationItem> arr = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportActionBar().setTitle(arr.get(0).getArticle());
+                NewsFragment fragment = new NewsFragment();
+                fragment.setId(0);
+                callNewsFragment(fragment);
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        arr.add(new NavigationItem("VnExpress", R.drawable.icon_vnexpress));
+        arr.add(new NavigationItem("24h", R.drawable.icon_24h));
+        arr.add(new NavigationItem("Dân trí", R.drawable.icon_dantri));
+        arr.add(new NavigationItem("Thanh niên", R.drawable.icon_thanhnien));
+        arr.add(new NavigationItem("Người đưa tin", R.drawable.icon_ndt));
+        arr.add(new NavigationItem("BBC News", R.drawable.icon_abc));
+        lv = (ListView) findViewById(R.id.listview);
+        MyNavigationAdapter adapter  = new MyNavigationAdapter(this, R.layout.navigation_item, arr);
+        lv.setAdapter(adapter);
+
+        getSupportActionBar().setTitle(arr.get(0).getArticle());
+        NewsFragment fragment = new NewsFragment();
+        fragment.setId(0);
+        callNewsFragment(fragment);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                getSupportActionBar().setTitle(arr.get(i).getArticle());
+                NewsFragment fragment = new NewsFragment();
+                fragment.setId(i);
+                callNewsFragment(fragment);
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void callNewsFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.frame_layout, fragment);
+        transaction.commit();
     }
 }
