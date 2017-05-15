1613690e1fc0dc495706ab03354082ea62345a8d
PHUC-PC\Phuc <tichuotuglyboy@gmail.com>
2017-05-15 20:44:55 +0700

    Update no internet

--- app/src/main/java/com/phuc/tintuctonghop/FragmentNoInternet.java
+++ app/src/main/java/com/phuc/tintuctonghop/FragmentNoInternet.java
@@ -19,8 +19,11 @@ public class FragmentNoInternet extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_no_internet, container, false);
+		
         TextView tvError = (TextView) view.findViewById(R.id.tv_error);
         Button btRetry= (Button) view.findViewById(R.id.bt_retry);
+		
+		/**show warning dialog when devices has no internet*/
         tvError.setText("No Internet connection. Make sure that Wi-Fi or cellular data is turned on, then try again.");
         btRetry.setOnClickListener(new View.OnClickListener() {
             @Override
